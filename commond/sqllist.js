229d53ad58c9421478ae78bae9cb47c062dc3baf
Chittipong <chittipong@Korawits-MacBook-Pro.local>
2018-12-10 02:42:29 +0700

    debug

--- commond/sqllist.js
+++ commond/sqllist.js
@@ -19,6 +19,6 @@ export default {
     gettransactionbyphone: "SELECT * FROM transactions WHERE phonenumber=$1 AND status='inuse'",
     updateBoxInfo: "UPDATE boxes SET price=$2, size=$3 WHERE id=$1",
     deleteBox: "DELETE FROM boxes WHERE id=$1",
-    lockBox: "UPDATE status='lock' WHERE id=$1",
-    unlockBox: "UPDATE status='aviable' WHERE id=$1"
+    lockBox: "UPDATE boxes SET status='lock' WHERE id=$1",
+    unlockBox: "UPDATE boxes SET status='aviable' WHERE id=$1"
 }
